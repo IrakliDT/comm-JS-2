6b6f38534c2a53e87693c5239a8b4f3d3dffa732
Irakli DT <inhale51@gmail.com>
2023-02-27 19:57:21 +0400

    js

---
+++ Javascript/script.js
@@ -0,0 +1,72 @@
+
+// 1 ======================= odd numbers =======================
+
+// const people = ["Giorgi", "Irakli", "Anri", "salome", "Barbare", "Daviti"]
+// for (let index in people){
+//   console.log(people[index])
+// }
+
+
+
+
+// 2 ======================= function price=======================
+
+// function display(x,func){
+//   let message = func(x)
+//   document.write(message)
+// }
+
+// function infoMessage(price){
+//   if(price>50)
+//   return "Expensive price"
+//   if(price<51 && price>20)
+//   return "normal price"
+//   else(price<20)
+//   return "low price"
+// }
+
+// display(20, infoMessage)
+
+
+
+
+// 3 ======================= odd numbers =======================
+
+// const numbers = [ 15,53,22,198,10,28,16,70,33,951 ]
+// for (let i=0; i < numbers.length; i ++){
+//   if (numbers[i] % 2 !=0){
+//     console.log(numbers[i])
+//   }
+// }
+
+
+
+
+// 4 ======================= movie =======================
+
+
+// const movies = {
+//   cinema: "Cavea",
+//   adress: "Tbilisi City mall",
+//   languages: ["GEO" , "EN" , "RU"],
+//     premiere:{
+//       name:"The Godfather",
+//       IMBD:9.2,
+// },
+//  weekmovie: [
+//     { name: "inception", IMBD: 8.8 },
+//     { name: "The Departed", IMBD: 8.5 },
+//     { name: "The Prestige", IMBD: 8.6 },
+//   ],
+// };
+
+// document.write("<h3>მიმდინარე კვირის ფილმები</h3>");
+// console.log(movies.name);
+// for (let i = 0; i < movies.cinema.length; i++) {
+//   document.write(movies.cinema[i] );
+// }
+// document.write("<h3>შვეიცარიის ქალაქებია</h3>");
+// for (let i = 0; i < movies.weekmovie.length; i++) {
+//   document.write(movies.weekmovie[i].name + " <br/>");
+// }
+
